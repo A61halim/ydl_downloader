eb255efb9bd29feabc649751cddef4699dc71dc4
A61halim <62565296+A61halim@users.noreply.github.com>
2020-06-06 20:06:17 +0100

    Update playlist.py

--- src/playlist.py
+++ src/playlist.py
@@ -7,7 +7,7 @@ class Plist():
     def __init__(self, url):
         ydl_opts = {
             'format':'best',
-            'outtmpl': '/home/topo/Desktop/COURSE/%(playlist)s/%(playlist_index)s __%(title)s',
+            'outtmpl': '%(playlist)s/%(playlist_index)s __%(title)s',
             'yesplaylist':True,
         }
         with youtube_dl.YoutubeDL(ydl_opts) as pdl:
